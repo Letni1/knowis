8f85a2130c2e2c11e444e6cc65326e84b97e128b
max <letnidev@gmail.com>
2019-03-13 18:07:43 +0200

    added rest to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,6 +58,8 @@ DATABASES = {
 ROOT_URLCONF = 'config.urls'
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = 'config.wsgi.application'
+# REST_FRAMEWORK
+# ------------------------------------------------------------------------------
 
 # APPS
 # ------------------------------------------------------------------------------
