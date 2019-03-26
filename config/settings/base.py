d8954b313c1420b055ee066c9b5165777b3f3e10
Maksym Proskurin <letnidev@gmail.com>
2019-03-27 01:57:40 +0200

    merge

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,7 +48,7 @@ DATABASES = {
         'NAME': 'knowis',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'databse1',
+        'HOST': 'database1',
         'PORT': '5432',
     }
 }
