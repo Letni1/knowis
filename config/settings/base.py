a9dce228378c29202dae48ad431f4303d0ed89f4
max <letnidev@gmail.com>
2019-04-16 14:07:56 +0300

    changed pagination to 5

--- config/settings/base.py
+++ config/settings/base.py
@@ -295,7 +295,7 @@ CORS_ORIGIN_WHITELIST = (
 # REST_FRAMEWORK
 # ------------------------------------------------------------------------------
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
