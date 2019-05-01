9a529701cf5198bb31d3ddfba3c224e3b6129f68
Max <letniqq@localhost.localdomain>
2019-05-02 01:23:37 +0300

    added ud by slug

--- knowis/core/api_urls.py
+++ knowis/core/api_urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
             ),
     re_path(r'^questions/d/(?P<slug>.+)/$',
             view=q_views.UserQuestionGetUpdateDeleteBySlug.as_view(),
-            name='questions_by_slug'
+            name='questions_by_slug'),
     re_path(r'^questions/d/(?P<uuid>[-\w]+)/$',
             view=q_views.UserQuestionGetUpdateDeleteByUUID.as_view(),
             name='questions_delete'),
