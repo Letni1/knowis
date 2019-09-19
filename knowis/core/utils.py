77159477babcd030f51bc39c4e4b5259a8aa0afc
letniqq <letniqq@localhost.localdomain>
2019-09-19 22:36:06 +0300

    dry serializers

--- knowis/core/utils.py
+++ knowis/core/utils.py
@@ -3,9 +3,14 @@ from django.
 
 class RemovedIdSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        abstract = True
+
     def get_fields(self, *args, **kwargs):
         fields = super().get_fields(*args, **kwargs)
         request = self.context.get("request")
         if request is not None and not request.parser_context.get("kwargs"):
             fields.pop("id", None)
         return fields
+
+
